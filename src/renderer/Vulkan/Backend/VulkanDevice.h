f968bd3f17343f6e5da067bd5a7e2c5942d93fa2
metinis <denis.muraska@gmail.com>
2025-06-12 22:33:03 +0100

    macos and windows

--- src/renderer/Vulkan/Backend/VulkanDevice.h
+++ src/renderer/Vulkan/Backend/VulkanDevice.h
@@ -19,11 +19,17 @@ private:
 	vk::UniqueDevice m_LogicalDevice;
 	vk::Queue m_GraphicsQueue;
 	vk::Queue m_PresentQueue;
+#ifdef __APPLE__
 	static constexpr std::array<const char*, 2> s_deviceExtensions = {
 		VK_KHR_SWAPCHAIN_EXTENSION_NAME,
 		"VK_KHR_portability_subset"
-
 	};
+#else
+	static constexpr std::array<const char*, 1> s_deviceExtensions = {
+		VK_KHR_SWAPCHAIN_EXTENSION_NAME,
+	};
+#endif
+
 	void CreatePhysicalDevice();
 	void CreateLogicalDevice();
 	static const bool IsDeviceSuitable(vk::PhysicalDevice physicalDevice, vk::SurfaceKHR surface);
