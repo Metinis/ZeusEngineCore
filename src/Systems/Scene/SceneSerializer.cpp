28ea60f667600a5cdb149b005e2ae3005f6133b7
metinis <denis.muraska@gmail.com>
2025-12-08 02:14:44 -0500

    initial commit

--- src/Systems/Scene/SceneSerializer.cpp
+++ src/Systems/Scene/SceneSerializer.cpp
@@ -1,23 +1,46 @@
 #include "SceneSerializer.h"
 #include <yaml-cpp/yaml.h>
+#include <entt/entt.hpp>
 #include <ZeusEngineCore/Application.h>
 
-ZEN::SceneSerializer::SceneSerializer(Scene *scene) : m_Scene(scene) {
+using namespace ZEN;
+SceneSerializer::SceneSerializer(Scene *scene) : m_Scene(scene) {
 
 }
 
-bool ZEN::SceneSerializer::serialize(const std::string &path) {
+static void serializeEntity(YAML::Emitter& out, Entity entity) {
+    out << YAML::BeginMap;
+    out << YAML::Key << "Entity" << YAML::Value << "671526351623";
+
+    if(entity.hasComponent<TagComp>()) {
+        out << YAML::Key << "TagComponent";
+        out << YAML::BeginMap;
+        out << YAML::Key << "Tag" << YAML::Value << entity.getComponent<TagComp>().tag;
+        out << YAML::EndMap;
+    }
+
+    out << YAML::EndMap;
+}
+
+bool SceneSerializer::serialize(const std::string &path) {
     YAML::Emitter out;
     out << YAML::BeginMap;
     out << YAML::Key << "Scene" << YAML::Value << "Default";
-    out << YAML::Key << "Entities";
+    out << YAML::Key << "Entities" << YAML::BeginSeq;
 
+    for (auto entityID : m_Scene->m_Registry.storage<entt::entity>()) {
+        Entity entity = Entity(m_Scene, entityID);
+        serializeEntity(out, entity);
+    }
+
+
+    out << YAML::EndSeq;
     out << YAML::EndMap;
     std::ofstream fout(Application::get().getResourceRoot() + path);
     fout << out.c_str();
     return true;
 }
 
-bool ZEN::SceneSerializer::deserialize(const std::string &path) {
+bool SceneSerializer::deserialize(const std::string &path) {
     return false;
 }
